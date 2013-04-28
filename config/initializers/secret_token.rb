533b85d7cca0d2ae82cb8b0ef8d11a3d0bd68a95
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-04-27 22:40:25 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImageSite::Application.config.secret_token = 'a89112ea9566c67eabd6dba9c969d031dcef522b342ffef9f4612729478b27569115f0aa8ed1fd49a2d7098b7cd6710e14aebb622da72b9db966ae40bb07be2e'
